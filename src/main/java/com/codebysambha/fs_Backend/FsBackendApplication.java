9b86dbdf5088bbef90d81a18df1f0b8f42728a49
Sambhaji patil <patilsambha11@gmail.com>
2025-03-05 12:03:47 +0530

    initial commit

---
+++ src/main/java/com/codebysambha/fs_Backend/FsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codebysambha.fs_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codebysambha/fs_Backend/FsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codebysambha.fs_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
